650332023206715f94fc7f9ee327294460c6b5ad
Abhinag Naidu <abhi.naidu@reancloud.com>
2016-08-12 15:41:36 +0000

    Added Chapter 4 exercises

---
+++ chapter_2/exercise_2.rb
@@ -0,0 +1,5 @@
+puts 'What is your favorite numner?'
+favorite_number = gets.chomp
+new_number = favorite_number.to_i + 1
+puts 'Your bigger and better favorite number should be ' + new_number.to_s
+
